f07412840d5f3cb6f37fe00b75b194a94a5662d2
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-07-29 00:34:36 -0400

    mensajes de listo

--- resources/views/contacts/failure.blade.php
+++ resources/views/contacts/failure.blade.php
@@ -4,16 +4,16 @@
 
 
 @section('content')
-<main>
+<main class="message failure">
     <div class="container">
         <div class="row">
 
             <div class="col-md-5">
                 <img class="w-100" src="{{ asset('images/incorrect-logo.png') }}" alt="">
             </div>
-            <div class="col-md-7">
+            <div class="col-md-7 text">
 
-                <h3 class="text-centerk">¡Tu teléfono o correo ya fueron registrados!</h3>
+                <h3 class="text-center">¡Tu teléfono o correo ya fueron registrados!</h3>
                 <p class="text-center">Te reenviamos a tu correo electrónico los detalles de la propuesta de negocios y los requisitos para ser agente autorizado!</p>
 
                 <p class="text-center small">
--- resources/views/contacts/success.blade.php
+++ resources/views/contacts/success.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Exitoso')
 
 @section('content')
-<main>
+<main class="message success">
     <div class="container">
         <div class="row">
             <div class="col-md-5">
                 <img class="w-100" src="{{ asset('images/correct-logo.png') }}" alt="">
             </div>
-            <div class="col-md-7">
+            <div class="col-md-7 text">
                 <h3 class="text-center">¡Tu registro de datos fue un éxito!</h3>
                 <p class="text-center">Enviamos a tu correo los detalles de la propuesta de negocios y los requisitos para ser agentes autorizados!</p>
                 <p class="text-center small">
