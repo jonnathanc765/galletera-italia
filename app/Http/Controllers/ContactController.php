9e4bfa4a77f5c536fd45f7741b8c3ff40ffdbc36
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-08-05 10:24:39 -0400

    agentes autorizados de inter

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -39,7 +39,7 @@ class ContactController extends Controller
         // }
 
         // try {
-            Mail::to('jonnathan.c.765@gmail.com')->send(new ProviderMail($data));
+            Mail::to('agentes.autorizados@inter.com.ve')->send(new ProviderMail($data));
         // } catch (\Throwable $th) {
         //     return back()->withError('Ha ocurrido un error al enviar procesar tu registro, por favor intentalo de nuevo')->withInput($request->input());
         // }
