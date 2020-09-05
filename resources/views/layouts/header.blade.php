0a25ab6638e88c631b3eb1792716aa994ed75ecb
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-04 22:01:53 -0400

    base header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,43 @@
-<header>
-    <div class="container">
-        <a href="{{ route('home') }}">
-            <img src="{{ asset('images/main-logo.png') }}" alt="">
+
+<header class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav"
+            aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand d-none" href="#">
+            <img src="{{ asset('img/galleteraitalialogo.png') }}" height="30" width="30" alt="tag">
         </a>
-    </div>
+
+        <div class="collapse navbar-collapse" id="main-nav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <img src="{{ asset('img/galleteraitalialogo.png') }}" height="150" width="30" alt="tag">
+
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Galeria</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+    </nav>
+
 </header>
