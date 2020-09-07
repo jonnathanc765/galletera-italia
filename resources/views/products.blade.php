57b4c16ec23c2ac86e6bc9b89fdfe090595e05bd
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-07 11:44:10 -0400

    base carousel

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -9,7 +9,46 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        uhhh carusel aca
+        <div class="col-12 px-0">
+            <div class="carousel">
+                <div class="carousel__cell">
+                    <div class="carousel__cell-content" data-maria="vainilla">
+
+                        <p class="mb-2 h2">Maria Tentazione de vainilla</p>
+                        <small class="d-block h5">Galleta dulce tipo María sabor a vainilla
+                            Peso neto: 200 gr.
+                            </small>
+                        <button class="btn btn-primary mb-1">Obtener ahora</button>
+                    </div>
+                </div>
+                <div class="carousel__cell">
+                    <div class="carousel__cell-content" data-maria="chocolate">
+
+                        <p class="mb-2 h2">Maria Tentazione Chocolate</p>
+                        <small class="d-block h5">Galleta dulce tipo María sabor a chocolate
+                            Peso neto: 200 gr.
+                            </small>
+                        <button class="btn mb-1">Obtener ahora</button>
+                    </div>
+                </div>
+                <div class="carousel__cell">
+                    <div class="carousel__cell-content" data-maria="original">
+
+                        <p class="mb-2 h2">Galleta dulce tipo María sabor a vainilla</p>
+                        <small class="d-block h5">Peso neto: 150 gr.</small>
+                        <button class="btn mb-1">Obtener ahora</button>
+                    </div>
+                </div>
+
+          </div>
+
+        </div>
     </div>
 </div>
+@endsection
+
+
+@section('scripts')
+<script src="{{ asset('js/carousel-products.js') }}"></script>
+
 @endsection
