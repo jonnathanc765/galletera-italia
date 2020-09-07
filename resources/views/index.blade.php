70a4a0b7dbcb6627d14a1cecde9304a16135d25d
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-07 07:18:21 -0400

    parte de nosotros home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
             <button class="btn btn-secondary">Conoce nuestros productos</button>
         </div>
     </div>
-    <div class="row cards-wrapper position-relative">
+    <div class="row cards-wrapper">
 
         <div class="row cards-container">
             <div class="cards-container__card offset-2 col-2"><p>Nosotros</p></div>
@@ -24,17 +24,24 @@
             <div class="cards-container__card col-2"><p>Contacto</p></div>
         </div>
     </div>
-    <div class="row">
-        <p class="h2">Nosotros</p>
-        <p>Galletera Italia, C.A es una empresa de origen venezolano
-            fundada en el año 2015, dedicada a la fabricación,
-            comercialización y distribución de artículos de galletería con
-            alcance a nivel nacional. Actualmente
-             fabricante de marcas comerciales como: María Italia, su marca
-            estrella desde los inicios de la empresa y María Tentazione desde
-            el año 2019 en sus dos presentaciones en sabor vainilla y sabor
-            chocolate.</p>
-            <button class="btn btn-primary">Conocenos</button>
+    <div class="row us-container">
+        <div class="col-12 col-lg-6 us-container__text">
+
+            <p class="display-3">Nosotros</p>
+            <p>Galletera Italia, C.A es una empresa de origen venezolano
+                fundada en el año 2015, dedicada a la fabricación,
+                comercialización y distribución de artículos de galletería con
+                alcance a nivel nacional. Actualmente
+                 fabricante de marcas comerciales como: María Italia, su marca
+                estrella desde los inicios de la empresa y María Tentazione desde
+                el año 2019 en sus dos presentaciones en sabor vainilla y sabor
+                chocolate.</p>
+                <button class="btn btn-primary">Conocenos</button>
+        </div>
+        <div class="col-12 col-lg-6 position-relative">
+            <img class="img-fluid" src="{{ asset('img/galleta-shape.png') }}"  alt="galleta">
+
+        </div>
     </div>
 </div>
 @endsection
