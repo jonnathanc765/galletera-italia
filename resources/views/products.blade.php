045cc65e656b423de7faabf91f25e72066fe1367
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-07 16:10:26 -0400

    imagenes de producto

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -11,8 +11,8 @@
     <div class="row">
         <div class="col-12 px-0">
             <div class="carousel">
-                <div class="carousel__cell">
-                    <div class="carousel__cell-content" data-maria="vainilla">
+                <div class="carousel__cell" data-maria="vainilla">
+                    <div class="carousel__cell-content">
 
                         <p class="mb-2 h2">Maria Tentazione de vainilla</p>
                         <small class="d-block h5">Galleta dulce tipo María sabor a vainilla
@@ -21,8 +21,8 @@
                         <button class="btn btn-primary mb-1">Obtener ahora</button>
                     </div>
                 </div>
-                <div class="carousel__cell">
-                    <div class="carousel__cell-content" data-maria="chocolate">
+                <div class="carousel__cell" data-maria="chocolate">
+                    <div class="carousel__cell-content">
 
                         <p class="mb-2 h2">Maria Tentazione Chocolate</p>
                         <small class="d-block h5">Galleta dulce tipo María sabor a chocolate
@@ -31,8 +31,8 @@
                         <button class="btn mb-1">Obtener ahora</button>
                     </div>
                 </div>
-                <div class="carousel__cell">
-                    <div class="carousel__cell-content" data-maria="original">
+                <div class="carousel__cell" data-maria="original">
+                    <div class="carousel__cell-content">
 
                         <p class="mb-2 h2">Galleta dulce tipo María sabor a vainilla</p>
                         <small class="d-block h5">Peso neto: 150 gr.</small>
