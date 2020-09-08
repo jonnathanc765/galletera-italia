7d798c26149922a567b2949cb0b42880278c5cf4
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-08 08:14:05 -0400

    mas estilos y empezando script de movimiento animal

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -19,14 +19,45 @@
             <p class="h2">QUE ACOMPAÑAN <span>tus dulces momentos</span></p>
             <button class="btn btn-primary">Contactanos</button>
             <img class="galletas" src="{{ asset('img/montaña.png') }}" alt="galletas">
-            <img src="{{ asset('img/animalito.png') }}" alt="animal de galletas" class="animalito">
+            <img src="{{ asset('img/animalito.png') }}" id="animalito" alt="animal de galletas" class="animalito">
         </div>
     </div>
 </div>
 @endsection
 
-
 @section('scripts')
+
+<script>
+
+    var animalito = document.getElementById('animalito')
+
+    const io = new IntersectionObserver(entries => {
+
+
+      // Element enters the viewport
+      if(entries[0].intersectionRatio !== 0) {
+
+        // How much of the element is visible
+        console.log(entries[0].intersectionRatio)
+
+      // Element leaves the viewport
+      } else {
+          console.log('no vep')
+      }
+
+    }, {
+        // Call the observer, when the element enters the viewport,
+        // when 25%, 50%, 75% and the whole element are visible
+        // threshold: [0.5]
+    });
+
+
+    // Start observing .box
+    io.observe(animalito);
+
+
+</script>
+
 {{-- <script src="{{ asset('js/carousel-gallery.js') }}"></script> --}}
 
 @endsection
