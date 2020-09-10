db71c6de772115de50a92093184d8c0da925ffee
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-09 23:16:02 -0400

    configurando fontawesome

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,16 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faMapMarkerAlt,faEnvelope, faHeart,} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
+library.add(faMapMarkerAlt,faEnvelope, faHeart, faLinkedinIn, faFacebookF, faInstagram)
+
+dom.watch()
+
+// const camera = icon({ prefix: 'fas', iconName: 'camera' })
+
+
+/* <i class="fab fa-linkedin-in"></i>
+<i class="fab fa-instagram"></i>
+<i class="fab fa-facebook-f"></i>
+<i class="far fa-envelope"></i>
+<i class="fas fa-heart"></i>
+<i class="fas fa-map-marker-alt"></i> */
