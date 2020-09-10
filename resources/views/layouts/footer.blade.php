9926f90ee1a92713de7782964590d69ebe9ea52b
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-09 23:22:44 -0400

    iconos en footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,16 +3,39 @@
         <div class="row">
 
             <div class="col-md-4 col-12">
-                <p>Ubicacion</p>
-                <address>Av. 02 Sur, Parque IndustrialCOSDZIEBA. Barinas, Venezuela.</address>
+                <div class="row">
+                    <div class="col-2"><i class="fas fa-map-marker-alt"></i></div>
+                    <div class="col-10">
+
+                        <p>Ubicacion</p>
+                        <address>Av. 02 Sur, Parque IndustrialCOSDZIEBA. Barinas, Venezuela.</address>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4 col-12">
-                <p>Contacto</p>
-                <p>nfo@galleteraitalia.com0273-5420707 / 5420461 / 5420392</p>
+                <div class="row">
+                    <div class="col-2"><i class="fas fa-envelope"></i></div>
+                    <div class="col-10">
+
+                        <p>Contacto</p>
+                        <p>info@galleteraitalia.com</p>
+                        <p>0273-5420707 / 5420461 / 5420392</p>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4 col-12">
-                <p>Redes sociales</p>
-                <p>3 iconos sociales aca</p>
+                <div class="row">
+                    <div class="col-2"><i class="fas fa-heart"></i></div>
+                    <div class="col-10">
+
+                        <p>Redes sociales</p>
+                        <p class="social-media">
+                            <i class="fab fa-linkedin-in"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-instagram"></i>
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
 
