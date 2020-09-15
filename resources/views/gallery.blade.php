1f3936193235235be3efd3fdc4989ffe3557eec3
Adrian Guerrero <adrianjguerrero87@gmail.com>
2020-09-15 07:54:37 -0400

    responsive animalito

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -111,22 +111,51 @@
 
     document.addEventListener("DOMContentLoaded", function () {
 
+        let cuandoMover = 0
+        let breakMin=0
+        let breakMax=0
+        let wantBreak = false
+        let compensacion = 0
+
+        if (window.matchMedia('(max-width: 768px)').matches) {
+            breakMin=0
+            breakMax=0
+            wantBreak = false
+            cuandoMover = 0.6
+
+            compensacion = '60vw'
+        }
         if (window.matchMedia('(min-width: 992px)').matches) {
+            breakMin=-300
+            breakMax=20
+            cuandoMover = 0.86
+            wantBreak = true
+
+            compensacion = '30vw'
+        }
 
             const animalito = document.getElementById('animalito')
             const container = document.querySelector('.slogan-container')
 
             const io = new IntersectionObserver(entries => {
 
-
+                console.log(breakMin,breakMax,cuandoMover,wantBreak);
                 // Element enters the viewport
                 if (entries[0].intersectionRatio !== 0) {
 
-                    // How much of the element is visible
 
-                    animalito.style.setProperty('--posX', `${entries[0].boundingClientRect.top * -1}px`)
+                    let value = entries[0].boundingClientRect.top * -1
 
-                    // Element leaves the viewport
+                    if ((value < breakMin || value > breakMax) && wantBreak) return
+
+                    if (entries[0].intersectionRatio >= cuandoMover) value = `${value}px + ${compensacion}`
+                    else value = `${value}px`
+
+                    console.log(entries[0].intersectionRatio)
+                    console.log(value);
+
+                    animalito.style.setProperty('--posX', `${value}`)
+
                 }
 
             }, {
@@ -138,7 +167,6 @@
 
             // Start observing .box
             io.observe(container);
-        }
 
     })
 
