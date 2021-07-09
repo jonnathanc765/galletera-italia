115e1e950fc3f87278b032b73d2770f6a691b22e
Adrian Guerrero <adrianjguerrero87@gmail.com>
2021-07-08 22:59:34 -0400

    agregado otros productos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -11,7 +11,7 @@
     <div class="row">
         <div class="col-12 px-0">
             <div class="carousel">
-                <div class="carousel__cell" data-maria="vainilla">
+                <div class="carousel__cell blue-color" data-maria="vainilla">
                     <div class="carousel__cell-content">
 
                         <p class="mb-2 h2">Maria Tentazione de vainilla</p>
@@ -22,7 +22,7 @@
                     </div>
                 </div>
 
-                <div class="carousel__cell" data-maria="chocolate">
+                <div class="carousel__cell red-color" data-maria="chocolate">
                     <div class="carousel__cell-content">
 
                         <p class="mb-2 h2">Maria Tentazione Chocolate</p>
@@ -39,6 +39,43 @@
                         <p class="mb-2 h2">Galleta dulce tipo María sabor a vainilla</p>
                         <small class="d-block h5">Peso neto: 150 gr.</small>
                         <button data-toggle="modal" data-target="#modal-galleta-original"  class="btn mb-1">Saber más</button>
+                    </div>
+                </div>
+
+
+                <div class="carousel__cell red-color" data-maria="tentazione-grande">
+                    <div class="carousel__cell-content">
+
+                        <p class="mb-2 h2">Galleta María Tentazione molida y triturada sabor a chocolate</p>
+                        <small class="d-block h5">Peso neto: 2.5 kg</small>
+                        <button data-toggle="modal" data-target="#modal-galleta-tentazione-grande"  class="btn mb-1">Saber más</button>
+                    </div>
+                </div>
+
+                <div class="carousel__cell blue-color" data-maria="vallina-grande">
+                    <div class="carousel__cell-content">
+
+                        <p class="mb-2 h2">Galleta María Tentazione molida y triturada sabor a vainilla</p>
+                        <small class="d-block h5">Peso neto: 2.5 kg</small>
+                        <button data-toggle="modal" data-target="#modal-galleta-vanilla-grande"  class="btn mb-1">Saber más</button>
+                    </div>
+                </div>
+
+                <div class="carousel__cell red-color" data-maria="tentazione-pequena">
+                    <div class="carousel__cell-content">
+
+                        <p class="mb-2 h2">Galleta María Tentazione molida y triturada sabor a chocolate</p>
+                        <small class="d-block h5">Peso neto: 500 g</small>
+                        <button data-toggle="modal" data-target="#modal-galleta-tentazione-pequena"  class="btn mb-1">Saber más</button>
+                    </div>
+                </div>
+
+                <div class="carousel__cell blue-color" data-maria="vanilla-pequena">
+                    <div class="carousel__cell-content">
+
+                        <p class="mb-2 h2">Galleta María Tentazione molida y triturada sabor a vainilla</p>
+                        <small class="d-block h5">Peso neto: 500 g</small>
+                        <button data-toggle="modal" data-target="#modal-galleta-vanilla-pequena"  class="btn mb-1">Saber más</button>
                     </div>
                 </div>
 
@@ -109,6 +146,95 @@
       </div>
     </div>
 </div>
+
+<div class="modal fade" id="modal-galleta-tentazione-grande" tabindex="-1" role="dialog" aria-labelledby="modal-galleta-tentazione-grande-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal-galleta-tentazione-grande-title">Galleta María Tentazione molida y triturada sabor a chocolate</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img class="img-fluid d-block mx-auto" height="350px" src="{{asset('img/tentazione-molida-grande.jpg')}}" alt="María Italia">
+            <p class="mb-1">Galleta María Tentazione molida y triturada sabor a chocolate</p>
+            <p class="mb-1"> <b> Peso neto:</b> 2.5 kg</p>
+            <p class="mb-1"> <b> Vida útil:</b> 12 meses.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="{{route('contact')}}" class="btn btn-primary">Obtener al mayor</a>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-galleta-vanilla-grande" tabindex="-1" role="dialog" aria-labelledby="modal-galleta-vanilla-grande-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal-galleta-vanilla-grande-title">Galleta María Tentazione molida y triturada sabor a vainilla</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img class="img-fluid d-block mx-auto" height="350px" src="{{asset('img/vanilla-molida-grande.jpg')}}" alt="María Italia">
+            <p class="mb-1">Galleta María Tentazione molida y triturada sabor a vainilla</p>
+            <p class="mb-1"> <b> Peso neto:</b> 2.5 kg</p>
+            <p class="mb-1"> <b> Vida útil:</b> 12 meses.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="{{route('contact')}}" class="btn btn-primary">Obtener al mayor</a>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-galleta-vanilla-pequena" tabindex="-1" role="dialog" aria-labelledby="modal-galleta-vanilla-pequena-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal-galleta-vanilla-pequena-title">Galleta María Tentazione molida y triturada sabor a vainilla</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img class="img-fluid d-block mx-auto" height="350px" src="{{asset('img/vanilla-pequena.png')}}" alt="María Italia">
+            <p class="mb-1">Galleta María Tentazione molida y triturada sabor a vainilla</p>
+            <p class="mb-1"> <b> Peso neto:</b> 500 g</p>
+            <p class="mb-1"> <b> Vida útil:</b> 12 meses.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="{{route('contact')}}" class="btn btn-primary">Obtener al mayor</a>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-galleta-tentazione-pequena" tabindex="-1" role="dialog" aria-labelledby="modal-galleta-vanilla-tentazione-title" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modal-galleta-vanilla-tentazione-title">Galleta María Tentazione molida y triturada sabor a chocolate</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <img class="img-fluid d-block mx-auto" height="350px" src="{{asset('img/tentazione-pequena.png')}}" alt="María Italia">
+            <p class="mb-1">Galleta María Tentazione molida y triturada sabor a chocolate</p>
+            <p class="mb-1"> <b> Peso neto:</b> 500 g</p>
+            <p class="mb-1"> <b> Vida útil:</b> 12 meses.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="{{route('contact')}}" class="btn btn-primary">Obtener al mayor</a>
+        </div>
+      </div>
+    </div>
+</div>
+
 
 <div class="modal fade" id="modal-galleta-original" tabindex="-1" role="dialog" aria-labelledby="modal-galleta-original-title" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
